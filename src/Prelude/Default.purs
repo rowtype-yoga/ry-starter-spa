1ea1984c28cad7166eff75bb3ea61903053f8958
Mark Eibes <mark.eibes@gmail.com>
2020-07-18 01:45:18 +0200

    Add Preludes

---
+++ src/Prelude/Default.purs
@@ -0,0 +1,11 @@
+module Prelude.Default
+  ( module Prelude
+  , module Data.Maybe
+  , module Effect
+  , module Effect.Class
+  ) where
+
+import Prelude
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Class (liftEffect)
