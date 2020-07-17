c51be85f389c673a4c805b9f7e4fedc3c64f8f72
Mark Eibes <mark.eibes@gmail.com>
2020-07-18 00:36:08 +0200

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
     }),
   ],
 
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+
+
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
