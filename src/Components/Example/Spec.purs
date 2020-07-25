300e74015727465a9b296b39bcf71fda3b3c8cea
Mark Eibes <mark.eibes@gmail.com>
2020-07-25 11:14:39 +0200

    Modify spec to work with emotionjs

--- src/Components/Example/Spec.purs
+++ src/Components/Example/Spec.purs
@@ -5,15 +5,14 @@ import Components.Example as Example
 
 spec ∷ Spec Unit
 spec =
-  after_ cleanup
-    $ describe "A simple counter" do
-        it "renders without errors" do
-          { findByText } <- renderComponent Example.component { initialValue: 0 }
-          elem <- findByText "Increment: 0"
-          elem `textContentShouldEqual` "Increment: 0"
-        it "increments when clicked" do
-          { findByText } <- renderComponent Example.component { initialValue: 3 }
-          elem <- findByText "Increment: 3"
-          elem `textContentShouldEqual` "Increment: 3"
-          fireEventClick elem
-          elem `textContentShouldEqual` "Increment: 4"
+  after_ cleanup do
+    describe "A simple counter" do
+      it "renders without errors" do
+        { findByText } <- renderComponent Example.component { initialValue: 0 }
+        elem <- findByText "Increment: 0"
+        elem `textContentShouldEqual` "Increment: 0"
+      it "increments when clicked" do
+        { findByText } <- renderComponent Example.component { initialValue: 3 }
+        elem <- findByText "Increment: 3"
+        fireEventClick elem 
+        void $ findByText "Increment: 4"
