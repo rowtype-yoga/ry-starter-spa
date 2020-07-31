5e8745d13d15e0bc3c69bebca9ee241216319efc
Mark Eibes <mark.eibes@gmail.com>
2020-07-31 17:25:19 +0200

    Add gzip compressed bundle

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,9 +3,13 @@
 const path = require("path");
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+
+  plugins: [new CompressionPlugin()],
+
 
   // The JavaScript file to be injected into the HTML file
   entry: path.resolve(__dirname, "index.prod.js"),
